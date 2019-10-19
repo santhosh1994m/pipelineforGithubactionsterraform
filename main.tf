a61551c0617bb92ac30bbea9952dcfb8090c50a2
santhosh1994m <45985363+santhosh1994m@users.noreply.github.com>
2019-10-19 18:28:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 provider "aws" {
-region = "us-west-2"
-version = ~>3
+  version = "~> 2.0"
+  region  = "us-east-2"
 }
-resource "aws_vpc" "my_vpc" {
-name = "santhosh"
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
